14e743061773ba34d5df62a326b86f846ff49ea5
Zuhaib Ali <ezuhaib@gmail.com>
2014-03-02 20:43:48 +0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchkickIssue139::Application.config.secret_token = '080744009026c5783a3af9e5989e826ec44bcc50290c722f9c9e621d22f144044165bac9e306cdc32cea97d797458a1fb86a170ddbd4d35bd57abe6dbd979985'
